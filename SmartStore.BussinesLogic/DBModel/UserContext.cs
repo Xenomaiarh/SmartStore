d5155c8b05e33d0b905187be9f1d15b453fa2660
Xenomaiarh <Xenomaiarh@XENOMAIARH-NOTE>
2024-04-17 12:10:42 +0300

    db

---
+++ SmartStore.BussinesLogic/DBModel/UserContext.cs
@@ -0,0 +1,20 @@
+﻿using SmartStore.Domain.Entities.User;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SmartStore.BusinessLogic.DBModel.Seed
+{
+    public class UserContext : DbContext
+    {
+        public UserContext() : base(nameOrConnectionString: "name=SmartStore")
+        {
+
+        }
+
+        public virtual DbSet<DBUser> Users { get; set; }
+    }
+}
